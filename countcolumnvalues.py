beabe3daaee0d7e9d6de4bddd8445b165cd80282
muneer78 <muneer@duck.com>
2023-02-15 11:09:04 -0600

    Create countcolumnvalues.py

---
+++ countcolumnvalues.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+df = pd.read_csv('carrier.csv', header=0)
+
+output=df.count(axis=0, level=None, numeric_only=False)
+
+output.to_csv('result.csv', index=False)
