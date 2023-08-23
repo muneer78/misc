28cbdef4d6833603f6a3889a6169cd1eaab2743b
muneer78 <muneer@duck.com>
2023-08-23 08:06:04 -0500

    Update restaurantchooser.py

--- restaurantchooser.py
+++ restaurantchooser.py
@@ -1,90 +1,29 @@
-"""
-Use random to shuffle items
-"""
-
 import random
 
-low = [
-    "Strip's Chicken",
-    "Panera",
-    "Chipotle",
-    "McDonald's",
-    "Sonic",
-    "Burger King",
-    "Wendy's",
-    "Taco Bell",
-    "DQ",
-    "Pizza Street",
-    "Wing Street",
-    "Little Caesar's",
-    "Pancho's",
-]
-mid = [
-    "ABC Cafe",
-    "Hong Kong Star",
-    "Q39",
-    "Bo Ling's",
-    "Cheddar's",
-    "Kulture Kurry",
-    "Fasika",
-    "El Salvadoreno",
-    "Nick & Jake's",
-    "Hu Hot",
-    "Mi Ranchito",
-    "Salty Iguana",
-    "Applebee's",
-    "Elsa's",
-    "Thai House",
-    "Mai Thai",
-    "India Palace",
-    "Macaroni Grill",
-    "Old Chicago",
-    "Red Lobster",
-    "Sushi Mido",
-    "Lucky Wok",
-    "Thai Place",
-    "KC Grill & Kabob",
-    "Zoe's Kitchen",
-    "Joe's Kansas City",
-    "Houlihan's",
-    "Red Door Grill",
-    "Pizza Hut",
-    "Minsky's",
-    "Buffalo Wild Wings",
-    "Domino's",
-    "Ni Hao Fresh",
-    "Em Chamas",
-    "Joy Wok",
-]
-high = [
-    "J. Gilbert's",
-    "Eddie V's",
-    "McCormick & Schmick's",
-    "Paisano's",
-    "Seafood Island",
-    "Argosy Buffet",
-    "Bristol",
-    "Cupini's",
-    "Garozzo's",
-    "Fogo de Chao",
-    "Sushi Train",
-    "Lulu's Noodle Shop",
-    "Burger Stand",
-    "Artego Pizza",
-    "Jade Garden",
-    "Buca di Beppo",
-    "Peachtree Buffet",
-    "Jazz",
-]
+name_list = ['low', 'mid', 'high']
 
-output_low = random.sample(low, k=3)
-output_mid = random.sample(mid, k=3)
-output_high = random.sample(high, k=3)
+user_input = input('Please input a name: ')
 
-MESSAGE_LOW = "Your low priced choices are: "
-MESSAGE_MID = "Your mid priced choices are: "
-MESSAGE_HIGH = "Your high priced or far away choices are: "
+if user_input not in name_list:
+    print('Please input a valid name')
+else:
+    low = ["Strip's Chicken", "Panera", "Chipotle", "McDonald's", "Sonic", "Burger King", "Wendy's", "Taco Bell", "DQ", "Pizza Street", "Wing Street", "Little Caesar's", "Pancho's"]
+    mid = ["ABC Cafe", "Hong Kong Star", "Q39", "Bo Ling's", "Cheddar's", "Kulture Kurry", "Fasika", "El Salvadoreno", "Nick & Jake's", "Hu Hot", "Mi Ranchito", "Salty Iguana", "Applebee's", "Elsa's", "Thai House", "Mai Thai", "India Palace", "Macaroni Grill", "Old Chicago", "Red Lobster", "Sushi Mido", "Lucky Wok", "Thai Place", "KC Grill & Kabob", "Zoe's Kitchen", "Joe's Kansas City", "Houlihan's", "Red Door Grill", "Pizza Hut", "Minsky's", "Buffalo Wild Wings", "Domino's", "Joy Wok"]
+    high = ["J. Gilbert's", "Eddie V's", "McCormick & Schmick's", "Paisano's", "Seafood Island", "Argosy Buffet", "Bristol", "Cupini's", "Garozzo's", "Fogo de Chao", "Sushi Train", "Lulu's Noodle Shop", "Burger Stand", "Artego Pizza", "Jade Garden", "Buca di Beppo", "Peachtree Buffet", "Jazz", "Em Chamas"]
 
-print(MESSAGE_LOW + ", ".join(output_low))
-print(MESSAGE_MID + ", ".join(output_mid))
-print(MESSAGE_HIGH + ", ".join(output_high))
+    output_low = random.sample(low, k=3)
+    output_mid = random.sample(mid, k=3)
+    output_high = random.sample(high, k=3)
+
+    message_low = f'Your low priced choices are: {", ".join(output_low)}'
+    message_mid = f'Your mid priced choices are: {", ".join(output_mid)}'
+    message_high = f'Your high priced or far away choices are: {", ".join(output_high)}'
+
+    if user_input == 'low':
+        print(message_low)
+    elif user_input == 'mid':
+        print(message_mid)
+    elif user_input == 'high':
+        print(message_high)
+    else:
+        print('Invalid input')
