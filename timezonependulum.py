36ca99c9869c8e84210ce4c4c2864bac942c13fe
muneer78 <muneer@duck.com>
2023-09-28 12:56:34 -0500

    Update timezonependulum.py

--- timezonependulum.py
+++ timezonependulum.py
@@ -1,43 +1,27 @@
 import pendulum
 
-# Define time zones and city names
-time_zones = [
-    ("PST", "Los Angeles"),
-    ("CST", "Kansas City"),
-    ("EST", "Montreal, Orlando, and Toronto"),
-    ("Europe/Istanbul", "Istanbul"),
-    ("Asia/Karachi", "Karachi"),
-    ("Asia/Dhaka", "Dhaka"),
-]
+central_tz = pendulum.timezone("US/Central")
 
-us_tz_map = {
-    "PST": "America/Los_Angeles",
-    "CST": "America/Chicago",
-    "EST": "America/New_York",
+time_zones = {
+    "PT": "US/Pacific",
+    "CT": "US/Central",
+    "ET": "US/Eastern",
+    "Istanbul": "Europe/Istanbul",
+    "Karachi": "Asia/Karachi",
+    "Dhaka": "Asia/Dhaka",
 }
 
-# Input datetime in Chicago time
-input_datetime = pendulum.from_format(
-    input("Enter the date and time in Central Time (YYYY-MM-DD HH:MM AM/PM): "),
-    "YYYY-MM-DD hh:mm A",
-)
-central_time_input = input_datetime.in_timezone("America/Chicago")
+input_str = input("Enter the date and time in Central Time (YYYY-MM-DD HH:MM AM/PM): ")
+input_dt = pendulum.from_format(input_str, "YYYY-MM-DD hh:mm A", tz=central_tz)
 
-# Get the specified time in the specified time zones
-specified_times = []
-for tz_abbr, city_name in time_zones:
-    if tz_abbr in us_tz_map:
-        tz = pendulum.timezone(us_tz_map[tz_abbr])
+output = []
+for tz_abbr, tz_name in time_zones.items():
+    tz = pendulum.timezone(tz_name)
+    dt = input_dt.in_timezone(tz)
+
+    if tz_abbr in ["PT", "CT", "ET"]:
+        output.append("{} {}".format(dt.format("h:mmA"), tz_abbr))
     else:
-        tz = pendulum.timezone(tz_abbr)
+        output.append("{} {}".format(dt.format("h:mmA"), tz_name.split("/")[-1]))
 
-    converted_time = central_time_input.in_timezone(tz)
-    formatted_time = converted_time.format("h:mmA")
-
-    if tz_abbr in ["PST", "CST", "EST"]:
-        specified_times.append(f"{formatted_time} {tz_abbr}")
-    else:
-        specified_times.append(f"{formatted_time} {city_name}")
-
-# Print results
-print(", ".join(specified_times))
+print(", ".join(output))
