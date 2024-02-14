50aeb240e992bb5fc39daa08518529f6218d48b7
muneer78 <muneer@duck.com>
2024-02-14 15:30:53 -0600

    Add files via upload

--- GroupBy.py
+++ GroupBy.py
@@ -2,10 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("System Closed Opps with Score Workbook.csv")
 
-# grouped_df = df["STC Lead Score"].value_counts()
-
-grouped_df = (
-    df.groupby("STC Lead Score")["STC Lead Score"].count().sort_values(ascending=False)
-)
+grouped_df = df["STC Lead Score"].value_counts().reset_index(name="Number")
 
 print(grouped_df)
+
+grouped_df.to_csv("GroupedDF.csv", index=False)
