6c2c3fa09e9438762795fea1cf7f46db6f8078a6
muneer78 <muneer@duck.com>
2024-03-21 15:11:40 -0500

    Delete draftorder.py

--- draftorder.py
+++
@@ -1,8 +0,0 @@
-"""
-Use numpy to shuffle items
-"""
-import numpy as np
-
-places = ["10th", "9th", "8th", "7th", "6th", "5th", "4th", "3rd", "2nd", "1st"]
-np.random.shuffle(places)
-print(places)
