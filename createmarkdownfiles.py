d883156e8b077f18900427ffdf475576aa4f1258
muneer78 <online@muneerahmad.com>
2024-08-06 21:48:15 -0500

    Added new scripts

--- createmarkdownfiles.py
+++ createmarkdownfiles.py
@@ -1,12 +1,55 @@
+# import os
+# from datetime import datetime, timedelta
+#
+# def create_markdown_file(category, title, imagename, date):
+#     # Format title for filename
+#     filename_title = '-'.join(title.lower().split())
+#
+#     # Create filename with date and formatted title
+#     filename = f"{date.strftime('%Y-%m-%d')}-{filename_title}.md"
+#
+#     # Create content with the specified pattern
+#     content = f"""---
+# categories: {category}
+# title: {title}
+# ---
+#
+# ![{imagename}](https://raw.githubusercontent.com/muneer78/muneer78.github.io/master/images/{imagename})
+# """
+#
+#     # Write content to the markdown file
+#     with open(filename, 'w') as file:
+#         file.write(content)
+#
+#     print(f"Markdown file '{filename}' created successfully.")
+#
+# # Example usage
+# tuples = [
+#     ("funny", "2 Chainz", "2chainz.png"),
+#     ("funny", "Ride Or Die", "3am.jpg"),
+# ]
+#
+# # Start date
+# start_date = datetime(2023, 1, 8)
+#
+# for i, (category, title, imagename) in enumerate(tuples):
+#     current_date = start_date + timedelta(days=i)
+#     create_markdown_file(category, title, imagename, current_date)
+
+'''
+Use this section for multiple files
+'''
+
 import os
-from datetime import datetime, timedelta
+import csv
+from datetime import datetime
 
 def create_markdown_file(category, title, imagename, date):
     # Format title for filename
     filename_title = '-'.join(title.lower().split())
 
     # Create filename with date and formatted title
-    filename = f"{date.strftime('%Y-%m-%d')}-{filename_title}.md"
+    filename = f"{date}-{filename_title}.md"
 
     # Create content with the specified pattern
     content = f"""---
@@ -14,7 +57,7 @@ categories: {category}
 title: {title}
 ---
 
-![{imagename}](https://raw.githubusercontent.com/muneer78/muneer78.github.io/master/images/{imagename}.jpg)
+![{imagename}](https://raw.githubusercontent.com/muneer78/muneer78.github.io/master/images/{imagename})
 """
 
     # Write content to the markdown file
@@ -23,26 +66,15 @@ title: {title}
 
     print(f"Markdown file '{filename}' created successfully.")
 
-# Example usage
-tuples = [
-    ("funny, black", "Black Dudes", "blackdudes.png"),
-    ("funny", "Miette", "Miette-all.jpg"),
-    ("funny", "Deep Magic", "deepmagic.png"),
-    ("politics", "Make Fun of Fascists", "fascists.png"),
-    ("funny", "Tooth Fairy", "bonesyouth.png"),
-    ("memes", "Professor Gorilla", "gorillaprof.png"),
-    ("work", "Performance Evaluations", "perfevals.png"),
-    ("memes", "Mamaw", "mamaw.jpg"),
-    ("funny", "Mascot", "mascots.jpg"),
-    ("funny", "What I Really Said", "motherf.jpg"),
-    ("memes", "Orca Song", "orca.jpg"),
-    ("funny", "Memphis Cat", "paulwall.jpeg"),
-    ("funny, work", "Awkward At Work", "worktrauma.jpg"),
-]
+# Read CSV and process rows
+csv_file_path = '/Users/muneer78/Downloads/muneericaposts.csv'  # Update this path
 
-# Start date
-start_date = datetime(2024, 5, 1)
+with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
 
-for i, (category, title, imagename) in enumerate(tuples):
-    current_date = start_date + timedelta(days=i)
-    create_markdown_file(category, title, imagename, current_date)
+    for row in reader:
+        category = row['category']
+        title = row['title']
+        imagename = row['filename']
+        date = row['date']
+        create_markdown_file(category, title, imagename, date)
