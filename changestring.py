1f40fbe63bec87a615590badcfbc9c97d645c9a4
muneer78 <online@muneerahmad.com>
2024-08-15 05:36:12 -0500

    Updated scripts

--- changestring.py
+++ changestring.py
@@ -14,7 +14,7 @@ for filename in os.listdir(directory):
                 content = file.read()
 
             # Replace instances of "greatlines" with "great-lines"
-            new_content = content.replace("greatlines", "great-lines")
+            new_content = content.replace("covid", "great-lines")
 
             # Write the updated content back to the file
             with open(file_path, 'w', encoding='utf-8') as file:
