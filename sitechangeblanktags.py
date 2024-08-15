9e111c7deae8ab2b62ba379033fae58b382490e2
muneer78 <online@muneerahmad.com>
2024-08-15 05:36:05 -0500

    Updated scripts

--- sitechangeblanktags.py
+++ sitechangeblanktags.py
@@ -1,7 +1,33 @@
+# import os
+#
+# # Set the directory to the path where your Markdown files are located
+# directory = '/Users/muneer78/Desktop/saved/'  # Replace this with the actual path
+#
+# # Iterate through all files in the directory
+# for filename in os.listdir(directory):
+#     if filename.endswith('.md'):
+#         filepath = os.path.join(directory, filename)
+#
+#         # Read the content of the file
+#         with open(filepath, 'r') as file:
+#             lines = file.readlines()
+#
+#         # Modify lines that only contain "tags:"
+#         modified_lines = [
+#             line if line.strip() != "title: Great Lines" else "title: "
+#             for line in lines
+#         ]
+#
+#         # Write the modified content back to the file
+#         with open(filepath, 'w') as file:
+#             file.writelines(modified_lines)
+#
+# print("Completed processing all Markdown files.")
+
 import os
 
 # Set the directory to the path where your Markdown files are located
-directory = '/Users/muneer78/quartz/content'  # Replace this with the actual path
+directory = '/Users/muneer78/quartz/content/'  # Replace this with the actual path
 
 # Iterate through all files in the directory
 for filename in os.listdir(directory):
@@ -12,9 +38,10 @@ for filename in os.listdir(directory):
         with open(filepath, 'r') as file:
             lines = file.readlines()
 
-        # Modify lines that only contain "tags:"
+        # Modify lines that contain "title: Great Lines"
         modified_lines = [
-            line if line.strip() != "tags:" else "tags: none\n"
+            line.replace("title: Great Lines", "title:").strip() + "\n"
+            if "title: Great Lines" in line else line
             for line in lines
         ]
 
