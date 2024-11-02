86e051d32018b5e23a5ed70e2ec35b900eb00937
jacquesfunk <reenum@gmail.com>
2024-11-01 19:11:09 -0500

    deleted scripts moved to utils folder

--- check-column-names.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-player_list_df = pd.read_csv("Player List.csv")
-rankings_df = pd.read_csv("laghezzaranks.csv")
-
-print("Player List columns:", player_list_df.columns)
-print("Rankings columns:", rankings_df.columns)
--- filter-unique.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_excel("sample-salesv3.xlsx")
-
-df["name"].unique()
-
-# Drop any records in the second field that are dupes and only select 1st 2 columns in dataframe
-df.drop_duplicates(subset=["account number", "name"]).iloc[:, [0, 1]]
