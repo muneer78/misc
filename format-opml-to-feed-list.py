2303613620bf2e6b359b4d262a7c5e5e970eb424
jacquesfunk <reenum@gmail.com>
2025-02-08 14:44:54 -0600

    Updated feed list and created script to parse OPML

---
+++ format-opml-to-feed-list.py
@@ -0,0 +1,13 @@
+import xml.etree.ElementTree as ET
+
+# Load and parse the OPML file
+tree = ET.parse('/Users/muneer78/Library/Mobile Documents/com~apple~CloudDocs/Documents/feeeed.opml')
+root = tree.getroot()
+
+# Open a new file to write the formatted feeds
+with open('/Users/muneer78/Library/Mobile Documents/com~apple~CloudDocs/Documents/formatted_feeds.txt', 'w') as f:
+    for outline in root.findall('.//outline'):
+        title = outline.get('title')
+        xmlUrl = outline.get('xmlUrl')
+        if title and xmlUrl:
+            f.write(f"'{title}': '{xmlUrl}',\n")
