cb44b084eff7ca9b7acb71bd1b3ecd1cd9e2608d
muneer78 <muneer78@MacBook-Pro.local>
2025-06-01 00:19:36 -0500

    Create autojump.zsh

---
+++ autojump.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+zstyle ':chpwd:*' recent-dirs-default yes
+zstyle ':completion:*' recent-dirs-insert always
+alias j=cdr
