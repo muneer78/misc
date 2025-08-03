7254a4b78814109f706eca0c8e6e4af89fc896ea
muneer78 <muneer78@MacBook-Pro.local>
2025-08-03 08:17:42 -0500

    updated scripts

--- scripts/sync-files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-rsync -avu /Users/muneer78/files/scripts/rss-feed-weekly.py /Users/muneer78/Documents/Projects/misc/
-
-rsync -avcru /Users/muneer78/files/scripts/ /Users/muneer78/Documents/Projects/misc/
-
-echo "Sync completed!"
