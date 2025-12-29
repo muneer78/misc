9d3b6667ed485ca2bde3db5651d1d4bfcbeec4ac
muneer78 <muneer78@MacBookPro.lan>
2025-12-29 06:57:21 -0600

    renamed script and updated another

--- images/convert-image-format.sh
+++ images/convert-image-format.sh
@@ -7,7 +7,7 @@ cd "$img_dir" || { echo "Directory not found: $img_dir"; exit 1; }
 # Find all images with the specified extensions (case-insensitive)
 find . -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.avif" -o -iname "*.heic" -o -iname "*.webp" \) | while read -r img; do
     filename="${img%.*}"
-    magick convert "$img" "$filename.png"
+    magick "$img" "$filename.png"
     echo "Converted $img to $filename.png"
     rm "$img"
     echo "Removed original file: $img"
