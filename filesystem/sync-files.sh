266bf578fa884b837542027dfa8c45d96f74f71c
muneer78 <muneer78@MacBookPro.lan>
2026-03-07 12:24:22 -0600

    removed sync to work emacs init.el

--- filesystem/sync-files.sh
+++ filesystem/sync-files.sh
@@ -3,6 +3,5 @@
 rsync -avu /Users/muneer78/Documents/GitHub/mun-ssg/*.py /Users/muneer78/Documents/Github/misc/text/
 rsync -avu /Users/muneer78/files/data '/Users/muneer78/Library/CloudStorage/GoogleDrive-reenum@gmail.com/My Drive/files/data/'
 rsync -avu /Users/muneer78/files/personal/pictures '/Users/muneer78/Library/CloudStorage/GoogleDrive-reenum@gmail.com/My Drive/files/pictures/'
-rsync -avu /Users/muneer78/.emacs.d/init.el /Users/muneer78/Documents/GitHub/dotfiles-jf/init.el
 
 echo "Sync completed!"
